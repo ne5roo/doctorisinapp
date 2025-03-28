79e1165091234342131a4b4dd65840716a8fc377
DnD17707 <ddiannegrace@gmail.com>
2025-03-28 12:17:01 -0400

    Update main.dart

--- main.dart
+++ main.dart
@@ -10,6 +10,7 @@ import 'help_page.dart'; // Import the HelpPage
 import 'settings_page.dart'; // Import the SettingsPage
 import 'package:provider/provider.dart';
 import 'providers/profile_image_provider.dart';
+import 'providers/theme_provider.dart';
 
 void main() async {
 
@@ -17,6 +18,7 @@ void main() async {
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => ProfileImageProvider()),
+        ChangeNotifierProvider(create: (_) => ThemeProvider()), // Add ThemeProvider
       ],
       child: const MyApp(),
     ),
@@ -28,22 +30,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context); // Access ThemeProvider
     return MaterialApp(
       title: 'Doctor is in App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        // Set AppBar theme globally
-        appBarTheme: AppBarTheme(
-          backgroundColor: const Color(0xFFD0F0C0), // Custom color for AppBar constant throughout the app
-        ),
-        // Set Scaffold background color globally
-        scaffoldBackgroundColor: const Color(0xFFD0F0C0), // Custom color for page background constant throughout the app
-        textTheme: TextTheme(
-          bodyLarge: TextStyle(color: Colors.black),  // Corrected text theme
-          bodyMedium: TextStyle(color: Colors.black), // Corrected text theme
-          bodySmall: TextStyle(color: Colors.black),  // Corrected text theme
-        ),
-      ),
+      theme: themeProvider.currentTheme, // Use currentTheme
       home: const SplashScreen(), // Initial splash or subscription page
       routes: {
         '/ageVerification': (context) => const AgeVerificationPage(), // Age Verification
@@ -67,10 +57,23 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context); // Access ThemeProvider
     return Scaffold(
-      appBar: AppBar(title: const Text('Welcome')),
-      body: const Center(
-        child: Text('You are successfully verified and logged in!'),
+      appBar: AppBar(
+        title: const Text(
+          'Welcome',
+          style: TextStyle(
+            fontFamily: 'ComicSansMS', // Set font style to ComicSansMS
+            fontWeight: FontWeight.bold, // Make the font bold
+          ),
+        ),
+        backgroundColor: themeProvider.currentTheme.appBarTheme.backgroundColor, // Use currentTheme for AppBar background color
+      ),
+      body: Container(
+        color: themeProvider.isDarkMode ? Colors.black : const Color(0xFFD0F0C0), // Apply background color
+        child: const Center(
+          child: Text('You are successfully verified and logged in!'),
+        ),
       ),
     );
   }
